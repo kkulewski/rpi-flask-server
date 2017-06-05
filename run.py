e930c1bf03ffde403e83c27113af0b3ccfdf806e
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-06-06 01:31:59 +0200

    Add write to file func

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import subprocess
 
 @app.route("/")
 def hello():
-    return "Hello!<br>address/r/ == read file"
+    return "Hello!<br>address/r/ == read file<br>address/w/variable == write variable to file"
 
 @app.route("/r/")
 def read():
@@ -13,6 +13,13 @@ def read():
 	data = file.read()
 	file.close()
 	return "%s" % data
+	
+@app.route("/w/<variable>")
+def write(variable):
+	cmd = "sudo echo %s\<br\> >> file.txt" % variable
+	os.system(cmd)
+	os.system("new value written to file")
+	return "value [ %s ] saved to file" % variable
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
