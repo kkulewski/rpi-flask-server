db342951a7582043fe10d354602b954e3b583c12
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-06-06 01:30:36 +0200

    Add read file func

--- run.py
+++ run.py
@@ -5,7 +5,14 @@ import subprocess
 
 @app.route("/")
 def hello():
-    return "Hello!"
+    return "Hello!<br>address/r/ == read file"
+
+@app.route("/r/")
+def read():
+	file = open("file.txt", "r")
+	data = file.read()
+	file.close()
+	return "%s" % data
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
